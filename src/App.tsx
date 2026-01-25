7ec98ce91a27a2fc5d9c8fedf34c3f90b5939ed3
pepperonipowered <pepperonipowered@gmail.com>
2026-01-25 23:51:42 +0800

    learning new stuff

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,25 @@ import SignupPage from "./components/auth/signup";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "./components/theme-provider";
 import { ModeToggle } from "./components/mode-toggle";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+    interface Register {
+        router: typeof router;
+    }
+}
 
 function App() {
     return (
-        <>
-            <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-                <Toaster />
-                <ModeToggle />
-                <LoginPage />
-            </ThemeProvider>
-        </>
+        <RouterProvider router={router} />
+        // <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        //     <Toaster />
+        //     <ModeToggle />
+        //     <LoginPage />
+        // </ThemeProvider>
     );
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-import { reactRouter } from "@react-router/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss(), reactRouter()],
+    plugins: [
+        tanstackRouter({
+            target: "react",
+            autoCodeSplitting: true,
+        }),
+        react(),
+        tailwindcss(),
+    ],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
